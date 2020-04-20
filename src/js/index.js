061be57605a8230494d895307a556927af39204b
Levshakov Nikolai <levshakov_nikk@bk.ru>
2020-04-20 15:09:15 +0300

    change mobile navBar and remove hover effects in mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,23 @@ window.addEventListener('DOMContentLoaded', (event)=> {
     console.log('hslkdgklsg');
     const download = document.querySelector('.pdf'),
             mobileMenu = document.querySelector('.menu'),
-            lines = document.querySelector('.lines');
+            lines = document.querySelector('.lines'),
+            navBar = document.querySelector('nav');
     
-    lines.addEventListener('click', function () {
-        mobileMenu.classList.toggle('mobile');
-        lines.classList.toggle('line_active');
-    })
+    navBar.addEventListener('click', (event)=> {
+        if (event.target && event.target == lines) {
+            console.log(event.target);
+            mobileMenu.classList.toggle('mobile');
+            lines.classList.toggle('line_active');
+        } else {
+            mobileMenu.classList.toggle('mobile');
+            lines.classList.toggle('line_active');
+        }
+    });
+    // lines.addEventListener('click', function () {
+    //     mobileMenu.classList.toggle('mobile');
+    //     lines.classList.toggle('line_active');
+    // })
 
     download.addEventListener('click', () => {
         location.href = 'CV.pdf';
